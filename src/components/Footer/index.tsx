e6b7c8ebafb26db7e52af53e95ea87524592f037
KanatipPuboonlap <kanatip.7425@gmail.com>
2023-04-25 13:04:59 +0700

    Test

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import "./Footer.scss";
+
+type Props = {};
+
+const Footer = ({}: Props) => {
+  return (
+    <footer>
+      <p>@Copyright 2023  Kanatip Puboonlap </p>
+    </footer>
+  );
+};
+
+export default Footer;
